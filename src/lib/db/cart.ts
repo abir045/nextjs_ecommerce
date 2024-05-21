b1e52e9a389fcbe200b897d8e84c08b8d319537c
unknown <abir045@gmail.com>
2024-05-21 13:51:16 +0600

    updated pagination and search functionality

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers";
 import { prisma } from "./prisma";
-import { Cart, Prisma } from "@prisma/client";
+import { Cart, CartItem, Prisma } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 
 export type CarWithProducts = Prisma.CartGetPayload<{
@@ -19,12 +21,27 @@ export type ShoppingCart = CarWithProducts & {
 
 
 export async function getCart(): Promise<ShoppingCart | null >{
-    const localCartId = cookies().get("localCartId")?.value
-    const cart = localCartId ?
+    const session = await getServerSession(authOptions)
+    
+    let cart: CarWithProducts | null = null
+
+    if(session) {
+
+        cart = await prisma.cart.findFirst({
+            where: {userId: session.user.id},
+            include: {items: {include: {product: true}}}
+        })
+
+    } else {
+
+        const localCartId = cookies().get("localCartId")?.value
+    cart = localCartId ?
     await prisma.cart.findUnique({
         where: {id: localCartId},
         include: {items: {include: {product: true}}}
     }) : null
+
+    }    
 
     if(!cart) {
         return null
@@ -38,17 +55,109 @@ export async function getCart(): Promise<ShoppingCart | null >{
 
 
 export async function createCart(): Promise<ShoppingCart> {
- const newCart = await prisma.cart.create({
-    data: {}
- })
+
+const session = await getServerSession(authOptions)
+
+let newCart: Cart ;
+
+if(session) {
+    newCart = await prisma.cart.create({
+        data: {userId: session.user.id}
+    })
+} else {
+
+    newCart = await prisma.cart.create({
+        data: {}
+     })
+     cookies().set('localCartId', newCart.id)
+
+    }
+
+
 
  // note: needs encryption + secure settings in production app
-cookies().set('localCartId', newCart.id)
+
 return {
     ...newCart,
     items: [],
     size: 0,
     subtotal: 0
 }
+
+}
+
+export async function mergeAnonymousCartIntoUserCart(userId: string) {
+
+    const localCartId = cookies().get("localCartId")?.value
+
+
+   const  localcart = localCartId ?
+    await prisma.cart.findUnique({
+        where: {id: localCartId},
+        include: {items: true}
+    }) : null
+
+    if(!localcart) return
+
+    const userCart = await prisma.cart.findFirst({
+        where: {userId},
+        include: {items: true}
+    })
+
+    await prisma.$transaction(async tx => {
+         if(userCart) {
+                const mergedCartItmes = mergeCartItems(localcart.items, userCart.items)
+
+                await tx.cartItem.deleteMany({
+                    where: {cartId: userCart.id}
+                })
+
+                await tx.cartItem.createMany({
+                    data: mergedCartItmes.map((item)=> ({
+                        cartId: userCart.id,
+                        productId: item.productId,
+                        quantity: item.quantity
+                    }))
+                })
+         } else {
+
+              await tx.cart.create({
+                data: {
+                    userId,
+                    items: {
+                        createMany: {
+                            data: localcart.items.map(item => ({
+                                productId: item.productId,
+                                quantity: item.quantity ,   
+                            }))
+                        }
+                    }
+                }
+
+                
+              })
+         }
+
+         await tx.cart.delete({
+            where: {id: localcart.id}
+         })
+       
+         cookies().set("localcartId", "" );
+
+    })
+}
+
+function mergeCartItems(...cartItems: CartItem[][]) {
+      return cartItems.reduce((acc, items) => {
+        items.forEach((item)=> {
+            const existingItem = acc.find((i) => i.productId === item.productId)
+            if(existingItem) {
+                existingItem.quantity += item.quantity ;
+            } else {
+                acc.push(item)
+            }
+        });
+      return acc
+    }, [] as CartItem[])
 
 }
